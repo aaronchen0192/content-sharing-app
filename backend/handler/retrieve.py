e53633d80d98bf066765a9cbe231dbc255346201
LOCOSA <1229390048@qq.com>
2023-07-02 00:51:13 -0400

    added retrieve handler

--- backend/handler/retrieve.py
+++ backend/handler/retrieve.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "arearsd123",
+            "message": "retrieve",
             # "location": ip.text.replace("\n", "")
         }),
     }
