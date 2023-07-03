42218f8f647a0b55b1c6d0c96798c51ad5b9697e
LOCOSA <1229390048@qq.com>
2023-07-03 00:52:04 -0400

    Added router and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,49 @@
-import { Button } from '@mui/material'
-import { useState } from 'react'
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Navigate,
+} from 'react-router-dom';
+import ShareSpace from './components/ShareSpace';
 
+const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+const generateId = () => {
+    let id = '';
+
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        id += characters[randomIndex];
+    }
+
+    return id;
+};
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/new',
+        element: <Navigate to={`/${generateId()}`} replace />,
+    },
+    {
+        path: '/:id',
+        element: <ShareSpace />,
+    },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <Button variant="contained">Hello World</Button>
-    </>
-  )
+    return <RouterProvider router={router} />;
 }
 
-export default App
+function Home() {
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    );
+}
+
+export default App;
