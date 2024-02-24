723443132c59e5a8b047787d32061d8bc9ec45f3
Wei Zheng <wjzheng0191@gmail.com>
2024-02-23 23:38:40 -0500

    Refactor BgEffect component to handle exceptions in useLayoutEffect

--- frontend/src/components/BgEffect/index.tsx
+++ frontend/src/components/BgEffect/index.tsx
@@ -6,33 +6,36 @@ import { useTheme } from '@mui/material';
 const BgEffect = () => {
   const theme = useTheme();
   useLayoutEffect(() => {
-    const opts =
-      theme.palette.mode === 'light'
-        ? {
-            skyColor: '#d4edfd',
-            cloudShadowColor: '#3d4a57',
-          }
-        : {
-            skyColor: '#000000',
-            cloudColor: '#353535',
-            cloudShadowColor: '#000000',
-            sunColor: '#121212',
-            sunGlareColor: '#121212',
-            sunlightColor: '#121212',
-          };
+    try {
+      const opts =
+        theme.palette.mode === 'light'
+          ? {
+              skyColor: '#d4edfd',
+              cloudShadowColor: '#3d4a57',
+            }
+          : {
+              skyColor: '#000000',
+              cloudColor: '#353535',
+              cloudShadowColor: '#000000',
+              sunColor: '#121212',
+              sunGlareColor: '#121212',
+              sunlightColor: '#121212',
+            };
 
-    const vantaEffect = Effect({
-      el: '#animated-bg',
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      speed: 1.25,
-      ...opts,
-    });
-
-    return () => {
-      vantaEffect.destroy();
-    };
+      const vantaEffect = Effect({
+        el: '#animated-bg',
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        speed: 1.25,
+        ...opts,
+      });
+      return () => {
+        vantaEffect.destroy();
+      };
+    } catch (e) {
+      console.error(e);
+    }
   }, [theme.palette.mode]);
 
   return <div id="animated-bg" />;
